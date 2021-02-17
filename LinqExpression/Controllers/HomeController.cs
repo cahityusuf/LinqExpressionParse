c3c01c85404964000764961344c1fb89d5007cb1
Cahit Yusuf KAFADAR <cahityusuf@gmail.com>
2021-02-17 23:27:01 +0300

    First Commit

---
+++ LinqExpression/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using LinqExpression.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqExpression.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Test([FromBody]List<SorguParametre> sorgu)
+        {
+
+            var testdata = new List<Ownr> {
+        //new Ownr{Name = "abc", Qty = 20}, // uncomment this to see it getting filtered out
+                new Ownr{Name = "abc", Qty = 2},
+                new Ownr{Name = "abcd", Qty = 11},
+                new Ownr{Name = "xyz", Qty = 40},
+                new Ownr{Name = "ok", Qty = 5},
+            };
+            Expression<Func<Ownr, bool>> func=null;
+
+            foreach (var item in sorgu)
+            {
+                func = Extentions.strToFunc<Ownr>(item.Key, item.Query, item.Value);
+            }
+ //func = Extentions.strToFunc<Ownr>("Qty", "<=", "10");
+ //           func = Extentions.strToFunc<Ownr>("Name", "==", "abc", func);
+
+            var result = testdata.Where(func.ExpressionToFunc()).ToList();
+
+            return View(result);
+        }
+        public class Ownr
+        {
+            public string Name { get; set; }
+            public int Qty { get; set; }
+        }
+
+
+
+
+    }
+}
---
+++ LinqExpression/wwwroot/js/SearchTest.js
@@ -0,0 +1,60 @@
+﻿var sorgu = new Array();
+
+$('#input1').on('change', function () {
+    var deger = $('#input1').val();
+    if (deger!=null) {
+        sorgu.push({
+            'Key': 'Name',
+            'Value': $('#input1').val(),
+            'Query': $('#sorguSecimi1').val()
+        });
+    }
+
+});
+
+$('#input2').on('change', function () {
+    var deger = $('#input2').val();
+    if (deger != null) {
+        sorgu.push({
+            'Key': 'Qty',
+            'Value': $('#input2').val(),
+            'Query': $('#sorguSecimi2').val()
+        });
+    }
+
+});
+
+$('#sorguSecimi1').on('change', function () {
+    var deger = $('#sorguSecimi1').val();
+    if (deger != null) {
+        sorgu.find(x => x.Key == 'Name').Query = $('#sorguSecimi1').val();
+    }
+
+});
+
+$('#sorguSecimi2').on('change', function () {
+    var deger = $('#sorguSecimi2').val();
+    if (deger != null) {
+        sorgu.find(x => x.Key == 'Qty').Query = $('#sorguSecimi2').val();
+    }
+
+});
+
+$('#gonder').on('click', function () {
+    console.log(JSON.stringify(sorgu));
+    $.ajax({
+        url: 'home/test',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(sorgu)
+    }).done(function (data) {
+        console.log(data);
+    }).fail(function (err) {
+        console.log(err);
+    });
+});
+//$(() => {
+
+//})
