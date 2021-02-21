1ed835dc06169bd55132ad89257dc6f835a4e649
Cahit Yusuf KAFADAR <cahityusuf@gmail.com>
2021-02-21 23:23:13 +0300

    JavaScrşpt-İmport-Calismasi

---
+++ LinqExpression/wwwroot/js/main.js
@@ -0,0 +1,7 @@
+﻿import { sayHi, sayBye, months, topla } from './say.js';
+
+
+$('#javascript').on('click', function () {
+    alert(topla(5,6))
+    //sayHi('John'); // Hello, John!
+});
---
+++ LinqExpression/wwwroot/js/say.js
@@ -0,0 +1,31 @@
+﻿// export an array
+export let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// export a constant
+export const MODULES_BECAME_STANDARD_YEAR = 2015;
+
+// export a class
+export class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+
+}
+
+
+
+
+function sayHi(user) {
+    alert(`Hello, ${user}!`);
+}
+
+function sayBye(user) {
+    alert(`Bye, ${user}!`);
+}
+
+function topla(sayi1, sayi2) {
+    return sayi1 + sayi2;
+}
+
+export { sayHi, sayBye, topla }; // a list of exported variables
